eec0bbde220ebfe5df87873c27f9360814a35ea3
kourosh-neyestani <kourosh.neyestani@gmail.com>
2020-02-23 14:27:43 +0330

    Added box, Added box style, Added output

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,26 +3,41 @@ import './style.css';
 
 function Home(props) {
 
-    const [blur, setBlur] = useState(0); // 0, 300
-    const [size, setSize] = useState(0); // -200, 200
+    const [blur, setBlur] = useState(30); // 0, 300
     const [inset, setInset] = useState(false); // true, false
-    const [opacity, setOpacity] = useState(0); // 0.00, 1.00
+    const [size, setSize] = useState(12); // -200, 200
     const [vertical, setVertical] = useState(0); // -200, 200
     const [horizontal, setHorizontal] = useState(0); // -200, 200
     const [boxColor, setBoxColor] = useState("#ffffff");
-    const [shadowColor, setShadowColor] = useState("##d93d64");
-    const [backgroundColor, setBackgroundColor] = useState("#ffffff");
+    const [shadowColor, setShadowColor] = useState("#e3e3e3"); // #d93d64"
+    const [wrapperColor, setWrapperColor] = useState("#ffffff");
+
+    const boxStyle = {
+        backgroundColor: `${boxColor}`,
+        boxShadow: `${inset ? "inset" : ""} ${horizontal}px ${vertical}px ${blur}px ${size}px ${shadowColor}`,
+    };
+
+    const wrapperStyle = {
+        backgroundColor: `${wrapperColor}`
+    }
 
     return (
         <section className="section">
             <div className="sidenav">
                 <div className="form">
-
                 </div>
             </div>
-            <div className="wrapper">
+            <div className="wrapper" style={wrapperStyle}>
                 <div className="center">
-                    <div className="element">
+                    <div className="box" style={boxStyle}>
+                        <div className="center">
+                            <div className="box-output">
+                                <p><b>CSS Output:</b></p>
+                                <p>-webkit-box-shadow: {boxStyle['boxShadow']};</p>
+                                <p>-moz-box-shadow: {boxStyle['boxShadow']};</p>
+                                <p>box-shadow: {boxStyle['boxShadow']};</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
